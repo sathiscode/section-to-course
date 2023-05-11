dbcff9f7fc6ff382b5b0875168b9bb94ebfb60a0
Sathis <sathiscode@users.noreply.github.com>
2023-05-12 03:49:35 +0530

    Update common.py

--- section_to_course/settings/common.py
+++ section_to_course/settings/common.py
@@ -7,4 +7,5 @@ def plugin_settings(settings):
     """Add django_object_actions to the installed apps so that its templates are loaded."""
     settings.INSTALLED_APPS += (
         'django_object_actions',
+        'SectionToCourseLink',
     )
